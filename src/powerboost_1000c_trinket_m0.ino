3e3e3d5cc2ecae61ffe8e30bf6cb7a344df60ee1
Methlal Pallewatta <mpall@mindspring.com>
2019-02-23 11:09:30 -0800

    Update powerboost_1000c_trinket_m0.ino

--- src/powerboost_1000c_trinket_m0.ino
+++ src/powerboost_1000c_trinket_m0.ino
@@ -1,7 +1,7 @@
 /* 
   This code is released under the MIT License.
   
-  Copyright 2019 Methlal Pallewatta
+  Copyright (c) 2019 Methlal Pallewatta
 
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
   and associated documentation files (the "Software"), to deal in the Software without restriction,
